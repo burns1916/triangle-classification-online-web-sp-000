c3aa7e647f65a96deed8e1ed89d589097f53450c
Ryan K Burns <ryankburns@gmail.com>
2020-10-14 17:36:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
   # write code here
+  attr_accessor :aide_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if @side_a == @side_b && @side_b == @side_c
+      :equilateral
+    end
+  end
+
+
+  class TriangleError < StandardError
+
+  end
 end
