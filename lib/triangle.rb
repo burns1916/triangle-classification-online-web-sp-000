c1a0c379c6230deef6d6966a1a7be9f833a63320
Ryan K Burns <ryankburns@gmail.com>
2020-10-14 17:38:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,8 @@ class Triangle
   def kind
     if @side_a == @side_b && @side_b == @side_c
       :equilateral
+    elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
+      :isosceles
     end
   end
 
