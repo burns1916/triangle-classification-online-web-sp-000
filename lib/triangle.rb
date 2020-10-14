9b6fe3fe83c505a79379863f30f3bd31aa580201
Ryan K Burns <ryankburns@gmail.com>
2020-10-14 17:51:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,6 @@ class Triangle
   end
 
   def kind
-    if @side_a == 0 && @side_b == 0 && @side_c == 0
-          begin
-            raise TriangleError
-          rescue TriangleError =>
-            error
-            puts error.message
-          end
     if @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
